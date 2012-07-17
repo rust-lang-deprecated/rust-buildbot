f949767dcca8f5c7849fd30b1a31c1b931355128
Graydon Hoare <graydon@mozilla.com>
2012-07-17 11:09:50 -0700

    restart

--- setup-slaves.sh
+++ setup-slaves.sh
@@ -6,5 +6,5 @@ do
         buildslave create-slave $slave localhost:9987 $slave "${pw}"
         echo "admin@rust-lang.org" >$slave/info/admin
         echo `hostname -f` >$slave/info/host
-		(cd $slave && buildslave start)
+		(cd $slave && buildslave stop; buildslave start)
 done
