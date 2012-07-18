bf22df54870c2311ecbbb0bb16cf8b35f82e41d3
Graydon Hoare <graydon@mozilla.com>
2012-07-18 13:17:51 -0700

    tweak

--- setup-slaves.sh
+++ setup-slaves.sh
@@ -5,6 +5,13 @@ do
         echo "creating slave: $slave"
         buildslave create-slave $slave localhost:9987 $slave "${pw}"
         echo "admin@rust-lang.org" >$slave/info/admin
-        echo `hostname -f` >$slave/info/host
-		(cd $slave && buildslave stop; buildslave start)
+        echo $HOSTNAME >$slave/info/host
+		case $MACHTYPE in
+			*-msys)
+				# service will start via service console
+				;;
+			*)
+				(cd $slave && buildslave stop; buildslave start)
+				;;
+		esac
 done
