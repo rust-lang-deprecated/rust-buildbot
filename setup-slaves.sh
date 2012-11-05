59309a23226661ac34913625aa74b77523fc776a
Graydon Hoare <graydon@mozilla.com>
2012-11-05 15:00:56 -0800

    One slave per slave. I know right?

--- setup-slaves.sh
+++ setup-slaves.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
-while read slave pw
-do
-        echo "creating slave: $slave"
-        buildslave create-slave $slave localhost:9987 $slave "${pw}"
-        echo "admin@rust-lang.org" >$slave/info/admin
-        echo $HOSTNAME >$slave/info/host
-		case $MACHTYPE in
-			*-msys)
+echo "Enter slave name: "
+read SLAVENAME
+
+echo "Enter slave password: "
+read -s PASSWORD
+
+echo "creating slave: $SLAVENAME"
+buildslave create-slave slave localhost:9987 "${SLAVENAME}" "${PASSWORD}"
+echo "admin@rust-lang.org" >slave/info/admin
+echo $HOSTNAME >slave/info/host
+case $MACHTYPE in
+	*-msys)
 				# service will start via service console
-				;;
-			*)
-				(cd $slave && buildslave stop; buildslave start)
-				;;
-		esac
-done
+		;;
+	*)
+		echo "starting slave..."
+		(cd slave && buildslave stop; buildslave start)
+		;;
+esac
