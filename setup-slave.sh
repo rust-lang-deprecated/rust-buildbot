e3cfcf094e9c56beb59f92c64fc6df1441677948
Graydon Hoare <graydon@mozilla.com>
2012-11-13 18:15:21 -0800

    Autoconfig on ec2, start stunnel in script, change to IP addresses in stunnel config.

--- setup-slave.sh
+++ setup-slave.sh
@@ -1,21 +1,41 @@
 #!/bin/sh
 
-echo "Enter slave name: "
-read SLAVENAME
+if [ ! -d slave ]
+then
+	echo "No slave/ dir, setting up..."
+	if hostname | grep -q '^ip-'
+	then
+		echo "Guessing we are on EC2, reading user-data"
+		read SLAVENAME PASSWORD <<EOF
+`curl -s http://169.254.169.254/latest/user-data`
+EOF
+	else
+		echo "Enter slave name: "
+		read SLAVENAME
+		echo "Enter slave password: "
+		read PASSWORD
+	fi
 
-echo "Enter slave password: "
-read PASSWORD
+	echo "creating slave: ${SLAVENAME:?}"
+	buildslave create-slave slave localhost:9987 "${SLAVENAME:?}" "${PASSWORD:?}"
+	echo "admin@rust-lang.org" >slave/info/admin
+	echo $HOSTNAME >slave/info/host
+fi
 
-echo "creating slave: $SLAVENAME"
-buildslave create-slave slave localhost:9987 "${SLAVENAME}" "${PASSWORD}"
-echo "admin@rust-lang.org" >slave/info/admin
-echo $HOSTNAME >slave/info/host
 case $MACHTYPE in
 	*-msys)
 				# service will start via service console
 		;;
 	*)
+		echo "starting stunnel..."
+		for s in stunnel4 stunnel
+		do
+			if which $s
+			then
+				$s rust-buildbot-slave-stunnel.conf || echo "stunnel startup failed, already running?"
+			fi
+		done
 		echo "starting slave..."
-		(cd slave && buildslave stop; buildslave start)
+		buildslave restart slave
 		;;
 esac
