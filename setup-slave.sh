fddfb70b5657b07f87533277af043fee7d6bd2ae
Brian Anderson <banderson@mozilla.com>
2014-05-23 21:03:48 +0000

    Get master address from slave userdata
    Conflicts:
    	setup-slave.sh

--- setup-slave.sh
+++ setup-slave.sh
@@ -2,8 +2,10 @@
 
 if hostname | grep -q '^ip-'
 then
+        # Some images take time for the user data to appear
+        sleep 1
 	echo "Guessing we are on EC2, reading user-data"
-	read SLAVENAME PASSWORD <<EOF
+	read SLAVENAME PASSWORD MASTER_ADDY <<EOF
 `curl -s http://169.254.169.254/latest/user-data`
 EOF
 else
@@ -11,6 +13,8 @@ else
 	read SLAVENAME
 	echo "Enter slave password: "
 	read PASSWORD
+	echo "Enter master address: "
+	read MASTER_ADDY
 fi
 
 rm -f slave/buildbot.tac* slave/twistd.* slave/info/admin slave/info/host
@@ -18,6 +22,9 @@ echo "(re)creating slave: ${SLAVENAME:?}"
 buildslave create-slave --force slave localhost:9987 "${SLAVENAME:?}" "${PASSWORD:?}"
 echo "admin@rust-lang.org" >slave/info/admin
 echo $HOSTNAME >slave/info/host
+
+cp rust-buildbot-slave-stunnel.conf rust-buildbot-slave-stunnel-final.conf
+echo "connect = ${MASTER_ADDY:?}" >> rust-buildbot-slave-stunnel-final.conf
 
 case $MACHTYPE in
 	*-msys)
@@ -29,7 +36,7 @@ case $MACHTYPE in
 		do
 			if which $s
 			then
-				$s rust-buildbot-slave-stunnel.conf || echo "stunnel startup failed, already running?"
+				$s rust-buildbot-slave-stunnel-final.conf || echo "stunnel startup failed, already running?"
 			fi
 		done
 		echo "starting slave..."
