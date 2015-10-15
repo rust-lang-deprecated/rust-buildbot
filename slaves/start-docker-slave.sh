9b44234fa7eed1ed2980449d713e843253d20e49
Alex Crichton <alex@alexcrichton.com>
2015-10-15 13:25:40 -0700

    Learn URL/branch from user-data

--- slaves/start-docker-slave.sh
+++ slaves/start-docker-slave.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-git clone https://github.com/alexcrichton/rust-buildbot
+url=`curl http://169.254.169.254/latest/user-data | tail -n +3 | head -n 1`
+branch=`curl http://169.254.169.254/latest/user-data | tail -n +4 | head -n 1`
+git clone $url --branch $branch
 export NODAEMON=1
 exec sh rust-buildbot/setup-slave.sh
