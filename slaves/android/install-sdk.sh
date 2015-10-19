9e5770a3ad8c5ac399ffe5fdc49f151733860e1d
Alex Crichton <alex@alexcrichton.com>
2015-10-19 16:28:17 -0700

    Refactor how the android image is built
    * Install everything as the rustbuild user
    * Move bulk to external scripts to cut down on intermediate docker images
    * Get all installs actually working

---
+++ slaves/android/install-sdk.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -ex
+
+# Prep the SDK and emulator
+#
+# Note that the update process requires that we accept a bunch of licenses, and
+# we can't just pipe `yes` into it for some reason, so we take the same strategy
+# located in https://github.com/appunite/docker by just wrapping it in a script
+# which apparently magically accepts the licenses.
+
+mkdir sdk
+curl http://dl.google.com/android/android-sdk_r24.4-linux.tgz | \
+    tar xzf - -C sdk --strip-components=1
+
+filter="platform-tools,android-18,android-21"
+filter="$filter,sys-img-x86-android-18"
+filter="$filter,sys-img-x86_64-android-21"
+filter="$filter,sys-img-armeabi-v7a-android-18"
+
+./accept-licenses.sh "android - update sdk -a --no-ui --filter $filter"
+
+echo "no" | android create avd \
+                --name arm-18 \
+                --target android-18 \
+                --abi armeabi-v7a
+echo "no" | android create avd \
+                --name x86-18 \
+                --target android-18 \
+                --abi x86
+echo "no" | android create avd \
+                --name x86_64-21 \
+                --target android-21 \
+                --abi x86_64
