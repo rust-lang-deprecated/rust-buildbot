783eeacef824a4f245ca9fd7b33104d223f5789d
Alex Crichton <alex@alexcrichton.com>
2015-10-19 17:01:53 -0700

    Not building musl on dist right now

--- slaves/dist/build_musl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -ex
-
-curl http://www.musl-libc.org/releases/musl-1.1.11.tar.gz | tar xzf -
-cd musl-1.1.11
-./configure --prefix=/rustroot/musl --disable-shared
-make -j10
-make install
