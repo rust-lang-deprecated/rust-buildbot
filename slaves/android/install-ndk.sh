ca1686fc08dde50d689948892171e7ac060a0352
Alex Crichton <alex@alexcrichton.com>
2015-11-22 11:24:24 -0800

    Use API level 21 for android slaves
    Currently don't have a reason not to!

--- slaves/android/install-ndk.sh
+++ slaves/android/install-ndk.sh
@@ -9,7 +9,7 @@ curl -O http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin
 chmod +x android-ndk-r10e-linux-x86_64.bin
 ./android-ndk-r10e-linux-x86_64.bin > /dev/null
 bash android-ndk-r10e/build/tools/make-standalone-toolchain.sh \
-        --platform=android-18 \
+        --platform=android-21 \
         --toolchain=arm-linux-androideabi-4.8 \
         --install-dir=/android/ndk-arm \
         --ndk-dir=/android/android-ndk-r10e \
@@ -21,7 +21,7 @@ bash android-ndk-r10e/build/tools/make-standalone-toolchain.sh \
         --ndk-dir=/android/android-ndk-r10e \
         --arch=arm64
 bash android-ndk-r10e/build/tools/make-standalone-toolchain.sh \
-        --platform=android-18 \
+        --platform=android-21 \
         --toolchain=x86-4.9 \
         --install-dir=/android/ndk-x86 \
         --ndk-dir=/android/android-ndk-r10e \
