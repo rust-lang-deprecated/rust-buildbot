7abd1efe347d96bd0c35f0b169c19dd6ba8522bd
Ralph Giles <giles@mozilla.com>
2015-11-25 09:10:45 -0800

    Build curl against the /rustroot copy of openssl.
    Without this, curl doesn't find openssl and is build without
    TLS support, contrary to the comment that we need this for
    downloading subsequent https resources.

--- slaves/dist/build_curl.sh
+++ slaves/dist/build_curl.sh
@@ -5,7 +5,7 @@ set -ex
 curl http://curl.haxx.se/download/curl-7.44.0.tar.bz2 | tar xjf -
 mkdir curl-build
 cd curl-build
-../curl-7.44.0/configure --prefix=/rustroot \
+../curl-7.44.0/configure --prefix=/rustroot --with-ssl=/rustroot \
       --disable-sspi --disable-gopher --disable-smtp --disable-smb \
       --disable-imap --disable-pop3 --disable-tftp --disable-telnet \
       --disable-manual --disable-dict --disable-rtsp --disable-ldaps \
