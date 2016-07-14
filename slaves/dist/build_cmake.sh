417ddbfb6db9d58364d57e610256749a331a507e
Alex Crichton <alex@alexcrichton.com>
2016-07-14 15:51:32 -0700

    Update CMake used across all bots
    Next LLVM upgrade will require CMake 3.4.3, and only Android is using a
    new-enough CMake version.

--- slaves/dist/build_cmake.sh
+++ slaves/dist/build_cmake.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-VERSION=3.3.2
-SHA256=e75a178d6ebf182b048ebfe6e0657c49f0dc109779170bad7ffcb17463f2fc22
+VERSION=3.6.0
+SHA256=fd05ed40cc40ef9ef99fac7b0ece2e0b871858a82feade48546f5d2940147670
 
 curl https://cmake.org/files/v${VERSION%\.*}/cmake-$VERSION.tar.gz | \
   tee >(sha256sum > cmake-$VERSION.tar.gz.sha256)                  | tar xzf -
