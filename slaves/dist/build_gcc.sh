941295cb1e9b2654c83d05f0e04c681f5f46452a
Ralph Giles <giles@mozilla.com>
2017-02-01 11:48:01 -0800

    Move gcc later in the build order.
    Building openssl and curl first lets us use TLS for all later
    downloads. The curl/openssl included with our Centos:5 base
    has compatibility problems with many modern websites.
    
    This slows the cycle time since we update openssl and curl
    more often than gcc, which was previously first since it's
    one of the slowest packages to build.

--- slaves/dist/build_gcc.sh
+++ slaves/dist/build_gcc.sh
@@ -6,7 +6,7 @@ VERSION=4.7.4
 SHA256=92e61c6dc3a0a449e62d72a38185fda550168a86702dea07125ebd3ec3996282
 
 yum install -y wget
-curl http://ftp.gnu.org/gnu/gcc/gcc-$VERSION/gcc-$VERSION.tar.bz2 | \
+curl https://ftp.gnu.org/gnu/gcc/gcc-$VERSION/gcc-$VERSION.tar.bz2 | \
   tee >(sha256sum > gcc-$VERSION.tar.bz2.sha256) | tar xjf -
 test $SHA256 = $(cut -d ' ' -f 1 gcc-$VERSION.tar.bz2.sha256) || exit 1
 
